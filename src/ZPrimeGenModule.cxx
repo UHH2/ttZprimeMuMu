12d65673b2011344a86860f769580533fe505058
Bjoern Tiedemann <bjoern.tiedemann@desy.de>
2019-06-03 17:32:06 +0200

    Add ana. moudule and config file for ZPrimeGen

---
+++ src/ZPrimeGenModule.cxx
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <memory>
+
+#include "UHH2/core/include/AnalysisModule.h"
+#include "UHH2/core/include/Event.h"
+#include "UHH2/ttZPrime/include/ZPrimeGen.h"
+//#include "UHH2/common/include/LQGen.h"
+#include "UHH2/common/include/PrintingModules.h"
+#include "UHH2/ttZPrime/include/ZPrimeGenHists.h"
+//#include "UHH2/common/include/LQGenHists.h"
+
+using namespace std;
+using namespace uhh2;
+
+namespace uhh2examples {
+/** \brief Example for calculating and accessing the TTbarGen interpretation
+ *
+ */
+class ZPrimeGenModule: public AnalysisModule {
+public:
+
+    explicit ZPrimeGenModule(Context & ctx);
+    virtual bool process(Event & event) override;
+
+private:
+  //std::unique_ptr<AnalysisModule> printer;
+  std::unique_ptr<AnalysisModule> ZPrimegenprod;
+  std::unique_ptr<Hists> h_ZPrimegenhists;
+  Event::Handle<ZPrimeGen> h_ZPrimegen;
+};
+
+
+ZPrimeGenModule::ZPrimeGenModule(Context & ctx){
+
+
+    //printer.reset(new GenParticlesPrinter(ctx));
+  ZPrimegenprod.reset(new ZPrimeGenProducer(ctx, "ZPrimeGen", false));
+  h_ZPrimegen = ctx.get_handle<ZPrimeGen>("ZPrimegGen");
+  h_ZPrimegenhists.reset(new ZPrimeGenHists(ctx, "ZPrimeGenHists"));
+}
+
+
+bool ZPrimeGenModule::process(Event & event) {
+  //printer->process(event);
+  ZPrimegenprod->process(event);
+
+    //const auto & LQLQbargen = event.get(h_LQgen);
+
+    cout << "Decay channel is "<< endl;
+
+    h_ZPrimegenhists->fill(event);
+
+    return true;
+}
+
+UHH2_REGISTER_ANALYSIS_MODULE(ZPrimeGenModule)
+}
