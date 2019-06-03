6933902a1f2b18a60c383b3729fa4e1a308ed792
Bjoern Tiedemann <bjoern.tiedemann@desy.de>
2019-06-03 17:27:46 +0200

    Code to take the ZPrime on gen. level

---
+++ include/ZPrimeGen.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include "UHH2/core/include/GenParticle.h"
+#include "UHH2/core/include/AnalysisModule.h"
+#include "UHH2/core/include/Event.h"
+
+#include <vector>
+
+
+/** \brief ttbar generator truth information
+ *
+ * Interpret a vector of GenParticle as ttbar event, providing easier access to
+ * the various components in the ttbar decay chain.
+ *
+ * The class can either be used directly by passing the genparticles vector;
+ * another option is to use the LQGenProducer (see below), which writes the
+ * result to the event container, where it can be accessed later.
+ */
+class ZPrimeGen {
+public:
+
+  /** construct from genparticles.
+   *
+   * The event should be an actual ttbar event, i.e.:
+   *   - there are exactly two tops: one top and one anti-top
+   *   - each top must have exactly two daughters
+   *   - one of the top daughters must be a W
+   *   - the Ws must have exactly two daughters
+   *
+   * Note that it is allowed that more particles than those belonging to the ttbar system
+   * are in genparts; those are ignored.
+   *
+   * In case one of these conditions is not fulfilled, the behavior
+   * depends on the throw_on_failure parameter: if it is true (the default), a runtime_error
+   * is thrown with an explanation what failed. If it is false, no exception is thrown, but
+   * not all contents is valid; most will return a 0 vector. The one thing guaranteed is that the
+   * decaychannel will be e_notfound. If using throw_on_failure = false, it is thus a good idea
+   * to check the decaychannel.
+   */
+  explicit ZPrimeGen(const std::vector<GenParticle> & genparts, bool throw_on_failure = true);
+
+  /*enum E_DecayChannel{
+    e_had,
+    e_ehad,
+    e_muhad,
+    e_tauhad,
+    e_ee,
+    e_mumu,
+    e_tautau,
+    e_emu,
+    e_etau,
+    e_mutau,
+    e_notfound
+    };*/
+
+  GenParticle ZPrime() const;
+  GenParticle MuZPrime() const;
+  GenParticle MuAntiZPrime() const;
+  //E_DecayChannel DecayChannel() const;
+
+  //bool IsTopHadronicDecay() const;
+  //bool IsAntiTopHadronicDecay() const;
+
+  //only for l+jets decays
+  /*GenParticle TopLep() const;
+  GenParticle TopHad() const;
+  GenParticle BLep() const;
+  GenParticle BHad() const;
+  GenParticle WLep() const;
+  GenParticle WHad() const;
+  GenParticle ChargedLepton() const;
+  GenParticle Neutrino() const;
+  GenParticle Q1() const;
+  GenParticle Q2() const; */
+
+
+ private:
+
+  GenParticle m_ZPrime;
+  GenParticle m_muZPrime;
+  GenParticle m_muAntiZPrime;
+
+
+
+  // E_DecayChannel m_type;
+};
+
+
+class ZPrimeGenProducer: public uhh2::AnalysisModule {
+public:
+    explicit ZPrimeGenProducer(uhh2::Context & ctx, const std::string & name = "ZPrimeGen", bool throw_on_failure = true);
+    virtual bool process(uhh2::Event & event) override;
+
+private:
+    uhh2::Event::Handle<ZPrimeGen> h_ZPrimeGen;
+    bool throw_on_failure;
+};
---
+++ src/ttZPrimeGen.cxx
@@ -0,0 +1,80 @@
+#include "UHH2/ttZPrime/include/ZPrimeGen.h"
+
+using namespace std;
+using namespace uhh2;
+
+ZPrimeGen::ZPrimeGen(const vector<GenParticle> & genparticles, bool throw_on_failure)/*: m_type(e_notfound)*/ {
+    int n_ZPrime = 0;
+    for(unsigned int i=0; i<genparticles.size(); ++i) {
+        const GenParticle & genp = genparticles[i];
+        //cout << genp.pdgId() << endl;
+        if (abs(genp.pdgId()) ==9000005){ // nciht nur die Zahl kann spaeter problem machen = ZPrime's
+            auto mu = genp.daughter(&genparticles, 1);
+            auto muAnti = genp.daughter(&genparticles, 2);
+            if(!mu || !muAnti){
+                if(throw_on_failure) throw runtime_error("ZPrimeGen: ZPrime has not ==2 daughters");
+                return;
+            }
+            if(abs(mu->pdgId()) != 13){
+                std::swap(mu, muAnti);
+            }
+            if(abs(mu->pdgId()) != 13){
+                if(throw_on_failure) throw runtime_error("ZPrimeGen: ZPrime has no Mu daughter");
+                return;
+            }
+
+            if(abs(muAnti->pdgId()) != 13){
+                if(throw_on_failure) throw runtime_error("ZPrimeGen: ZPrime has no MuAnti daughter");
+                return;
+            }
+            // now get W daughters:
+
+            // auto topd1 = top->daughter(&genparticles, 1);
+            // auto topd2 = top->daughter(&genparticles, 2);
+            // if(!topd1 || !topd2){
+            //     if(throw_on_failure) throw runtime_error("ZPrimeGen: top has not ==2 daughters");
+            //     return;
+            // }
+
+            // now that we collected everything, fill the member variables.
+            m_ZPrime = genp;
+            m_muZPrime = *mu;
+            m_muAntiZPrime = *muAnti;
+            ++n_ZPrime;
+            //cout << "Number of ZPrimes: " << n_ZPrime << endl;
+
+        }
+    }
+    if(n_ZPrime != 1){
+        if(throw_on_failure)  throw runtime_error("ZPrimeGen: did not find exactly one ZPrime in the event");
+        return;
+    }
+}
+
+
+GenParticle ZPrimeGen::ZPrime() const{
+    return m_ZPrime;
+}
+
+
+
+GenParticle ZPrimeGen::MuZPrime() const{
+    return m_muZPrime;
+}
+
+
+GenParticle ZPrimeGen::MuAntiZPrime() const{
+    return m_muAntiZPrime;
+}
+
+
+
+
+ZPrimeGenProducer::ZPrimeGenProducer(uhh2::Context & ctx, const std::string & name, bool throw_on_failure_): throw_on_failure(throw_on_failure_){
+    h_ZPrimeGen = ctx.get_handle<ZPrimeGen>(name);
+}
+
+bool ZPrimeGenProducer::process(Event & event){
+    event.set(h_ZPrimeGen, ZPrimeGen(*event.genparticles, throw_on_failure));
+    return true;
+}
