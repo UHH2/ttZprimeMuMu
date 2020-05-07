c8ffbeae93ed9f7be649bf7adadfc58c05a1f42a
Bjoern Tiedemann <bjoern.tiedemann@desy.de>
2020-05-07 11:51:37 +0200

    Clean up the muon selection

--- src/ttZPrimeControlModule.cxx
+++ src/ttZPrimeControlModule.cxx
@@ -26,15 +26,14 @@ namespace uhh2examples {
   private:
     bool is_mc;
     unique_ptr<AnalysisModule> mc_lumi_weight, mc_pu_reweight, mu_fake_rate_weight;
-    unique_ptr<Selection> nmu_tight_sel, mu1mu2_sel, m_mumu_sel;
+    unique_ptr<Selection> nmu_tight_sel, mu1mu2_sel, m_mumu_sel, mu3_loose_sel, mu3_tight_sel;
 
     unique_ptr<Hists> h_2mu;
     unique_ptr<Hists> h_3mu, h_3mu_loose, h_3mu_tight,  h_3mu_control, h_3mu_control_loose, h_3mu_control_tight;
     unique_ptr<Hists> h_bgen, h_3mu_loose_bgen, h_3mu_tight_bgen;
     unique_ptr<Hists> h_emu, hemu_control, h_emumu_tight,  h_emumu_tight_control;
-    unique_ptr<ElectronCleaner> electroncleaner;
     unique_ptr<MuonCleaner> mouncleaner_tight;
-    MuonId MuIdTight;
+    MuonId MuIdTight, MuIdLoose;
     ElectronId EleId;
   };
 
@@ -51,6 +50,7 @@ namespace uhh2examples {
         mu_fake_rate_weight.reset(new MuFakeRateWeight("/nfs/dust/cms/user/tiedemab/CMSSW_10_2_X/CMSSW_10_2_10/src/UHH2/ttZPrime/scripts/TTBar_FakeRateMap.txt",2));
 
         MuIdTight = AndId<Muon>(MuonID(Muon::CutBasedIdTight), PtEtaCut(30.0, 2.4), MuonIso(0.15));
+        MuIdLoose = AndId<Muon>(MuonID(Muon::CutBasedIdLoose), PtEtaCut(30.0, 2.4), MuonIso(0.15));
         mouncleaner_tight.reset(new MuonCleaner(MuIdTight));
 
 
@@ -58,7 +58,8 @@ namespace uhh2examples {
         nmu_tight_sel.reset(new NMuonSelection(2, -1, MuIdTight));
         mu1mu2_sel.reset(new NMuIDSelection(MuIdTight, 2));
         m_mumu_sel.reset(new MMuMUSelection(120.));
-
+        mu3_loose_sel.reset(new NMuonSelection(3, 3, MuIdLoose));
+        mu3_tight_sel.reset(new NMuonSelection(3, 3, MuIdTight));
 
 
         h_bgen.reset(new ttZPrimeGenBHists(ctx, "GenBQuarks"));
@@ -86,49 +87,46 @@ namespace uhh2examples {
 
   bool ttZPrimeControlModule::process(Event & event) {
 
-  if(is_mc)
-  {
-    mc_lumi_weight->process(event);
-    mc_pu_reweight->process(event);
-  }
+   if(is_mc)
+   {
+     mc_lumi_weight->process(event);
+     mc_pu_reweight->process(event);
+   }
 
 
 
-  if(!nmu_tight_sel->passes(event)) return false;
-  if(!mu1mu2_sel->passes(event)) return false;
-  if(!m_mumu_sel->passes(event)) return false;
+   if(!nmu_tight_sel->passes(event)) return false;
+   if(!mu1mu2_sel->passes(event)) return false;
+   if(!m_mumu_sel->passes(event)) return false;
 
- 
-  h_bgen->fill(event);
-  auto old_weight = event.weight;
+   h_2mu->fill(event);
+   h_bgen->fill(event);
+  
+  
+   if(!mu3_loose_sel->passes(event)) return false;
+   auto old_weight = event.weight;  
+   std::cout << "Original event weight: " << event.weight << "\n";
+   mu_fake_rate_weight->process(event);
+   std::cout << "Scaled event weight: " << event.weight << "\n";
+  
+   h_3mu_control_loose->fill(event);
+   h_3mu_loose->fill(event);
+   h_3mu_loose_bgen->fill(event);
+  
 
-  if(event.muons->size() == 3)
-  {
-//     std::cout << "Original event weight: " << old_weight << "\n";
-    mu_fake_rate_weight->process(event);
-//     std::cout << "Scaled event weight: " << event.weight << "\n";
-    h_3mu_control_loose->fill(event);
-    h_3mu_loose->fill(event);
-    h_3mu_loose_bgen->fill(event);
-  }
+   event.weight = old_weight;
+   std::cout << "Back scaled event weight: " << event.weight << "\n";
 
-  event.weight = old_weight;
-  mouncleaner_tight->process(event);
-  int NMuons = event.muons->size();
-  if(NMuons == 2)
-  {
-    h_2mu->fill(event);
-  }
-  if(NMuons == 3)
-  {
-    h_3mu_control_tight->fill(event);
-    h_3mu_tight->fill(event);
-    h_3mu_tight_bgen->fill(event);
-  }
+   if(!mu3_tight_sel->passes(event)) return false;  
+
+   h_3mu_control_tight->fill(event);
+   h_3mu_tight->fill(event);
+   h_3mu_tight_bgen->fill(event);
+  
 
 
 
-    return true;
+   return true;
 
 
   }
